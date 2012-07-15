7afda4fdcca6c738f796377c1d31d913cbc0c078
Erik Hedvall <erikwhedvall@gmail.com>
2012-07-15 18:50:08 +0200

    Added GPLv3 license

--- types/Background.js
+++ types/Background.js
@@ -1,3 +1,22 @@
+/*
+	Copyright 2012 Erik Hedvall.
+
+    This file is part of Parallax.js.
+
+    Parallax.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Parallax.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Parallax.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 Parallax.Background = function(element, parameters) {
 	this.target = element;
 	this.scrollObject = Parallax.getScrollObject(element);
